3de91349ce1a05c12b90fd1627c47f0ce4505fb1
Himanshu Jangid <iamahalofan@gmail.com>
2023-01-21 13:37:51 +0530

    📝 🚨 remove compiler warning and add doc for first function

---
+++ 1_first_program/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    println!("Hello, this is the  world!");
+    first_fun()
+}
+
+/// **What is rust Function?**
+///
+/// A rust function is collection of statements that helps us
+/// make our codebase more modular and readable.
+///
+fn first_fun() {
+    println!("this is the first function ")
+}
